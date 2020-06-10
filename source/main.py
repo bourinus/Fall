a9bae05642c3217779774b93d77b90e0f1ccb113
sh4d0w <mehdi.belahcene@outlook.fr>
2020-06-10 10:32:33 +0200

    setup(arg):

--- source/main.py
+++ source/main.py
@@ -11,7 +11,10 @@ from .exceptions import Fall_3
 from .exceptions import Fall_4
 from random import random
 
-
+def setup(argmn):
+    """
+    
+    """
 
 def hello(name="World"):
   return "Hello %s!" % name
