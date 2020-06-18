1046456e757841782fc0a7e4108d42d5e1c0e63e
sh4d0w <mehdi.belahcene@outlook.fr>
2020-06-18 23:34:23 +0200

    trying to fix...
    The Python interpreter limits the recursion limit so that infinite
    recursions are avoided.
    Triyng to handle
    Change-Id: I000000000000000000000000000000000000000
    
    Change-Id: I67d37ee71bee61df1c44d4a34277b1960223bf9f

--- source/define.py
+++ source/define.py
@@ -1,10 +1,12 @@
 # coding: utf-8
 ##change
 #import math
-
-from exceptions import Fall_2
-from exceptions import Fall_3
-from exceptions import Fall_4
+from .exceptions.Fall2 import *
+from .exceptions.Fall3 import *
+from .exceptions.Fall4 import *
+#from exceptions import Fall_4
+from builtins import str
+from source import exceptions
 #from pip._vendor.html5lib._ihatexml import charStringToList
 #from pip._internal.utils.compat import str_to_display
 #from builtins import str
@@ -19,47 +21,48 @@ class Define(object):
     """
     Define Class
     """
-    def __init__(self, defineInit=0, defineReturn=1, defineString="DefineInstance" ):
-        self.defineInit = defineInit
-        self.defineReturn = defineReturn
-        self.defineString = defineString
+    def __init__(self, define_init=0, define_return=1, define_string="DefineInstance" ):
+        self.define_init = define_init
+        self.define_return = define_return
+        self.define_string = define_string
     
     @property
-    def defineInit(self):
-        return self.__defineInit
+    def define_init(self):
+        return self.__define_init
      
-    @defineInit.setter 
-    def defineInit(self, newNum, ):
+    @define_init.setter 
+    def define_init(self, newNum, ):
         if isinstance(newNum, int) == False:
-            raise DefineException("Numerator must be an integer")
-        self.__defineInit = newNum
+            raise DefineException("Define should be an integer")
+        self.__define_init = newNum
+        self.__define_string = "Definine initiate..."
          
     @property
-    def defineReturn(self):
-        return self.defineReturn
+    def define_return(self):
+        return self.define_return
     
-    @defineReturn.setter 
-    def defineReturn(self, newDen):
+    @define_return.setter 
+    def define_return(self, newDen):
         if isinstance(newDen, int) == False:
-            raise DefineException("defineReturn must be an integer")
+            raise DefineException("Define should be an integer")
         if newDen == 0:
-            raise DefineException("defineReturn cannot be zero")
-        self.__defineReturn = newDen
+            raise DefineException("define_return cannot be zero")
+        self.__define_return = newDen
 
     @property
-    def defineString(self):
-        return self.defineReturn
+    def define_string(self):
+        return self.define_return
     
-    @defineString.setter 
-    def defineString(self, defineString):
-        if isinstance(defineString, str) == False:
-            raise DefineException("defineString must be an string")
-        if defineString == '':
-            raise DefineException("defineString can't be empty")
-        self.__defineString = defineString
+    @define_string.setter 
+    def define_string(self, define_string):
+        if isinstance(define_string, str) == False:
+            raise DefineException("define_string must be an string")
+        if define_string == '':
+            raise DefineException("define_string can't be empty")
+        self.__define_string = define_string
           
 
-    def define(self, defineInit):
+    def define(self, define_init):
         """
         Define is a paradox & repetition based function & an interface to other paradoxical functions.
         1 entry, 3 equality, 2 paradigms.
@@ -69,29 +72,29 @@ class Define(object):
         Always has 3 consecutive values equals.
         Always hides an important information: the 6th value is never returned.
         """
-        if isinstance(defineInit, str):
-                self.__defineReturn = "Some to write or do"
-                self.__defineString = "Someting telse"
-        if isinstance(defineInit, list):
-                self.__defineReturn = list #TO DO 
+        if isinstance(define_init, str):
+                self.__define_return = "Some to write or do"
+                self.__define_string = "Someting telse"
+        if isinstance(define_init, list):
+                self.__define_return = list #TO DO 
         
-        if defineInit == '':
-                self.__defineReturn = 0
-        s2 = defineInit 
+        if define_init == '':
+                self.__define_return = 0
+        s2 = define_init 
         for i in range(0, 3):
-                print(defineInit) 
-                print("-------")
+                self.__define_string =  self.__define_string +":"+str(i) 
+                #print("-------")
                 try:
-                        s1 = defineInit
-                        self.__defineReturn = (s1 ** i)
+                        s1 = define_init
+                        self.__define_return = (s1 ** i)
                         # if i!=3: print("###########")
-                except Fall_4 as e:
-                        Fall_4(e)
+                except exceptions.Fall_4:
+                        #(exceptions.Fall_4.A.__context__.__delete__(self))
                         break                        
                 pass
         #print("returned:",)
         #In Classes, doin't return set an attribute like following
-        self.__defineReturn = s2
+        self.__define_return = s2
     
 
 
