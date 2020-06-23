27ffadd0de2b9e7f0f0d15ad0aba2b085859f525
sh4d0w <mehdi.belahcene@outlook.fr>
2020-06-23 13:38:12 +0200

    latextopdf build generated_pdf.pdf venv is set 3.8
    Signed-off-by: sh4d0w <mehdi.belahcene@outlook.fr>

---
+++ docs/source/py2pdf.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+
+import os
+
+from latex import build_pdf
+
+def building_pdf():
+    # we need to supply absolute paths
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+
+    # we are adding an empty string to include the default locations (this is
+    # described on the tex manpage)
+    pdf = build_pdf(open('fall_texdoc.latex'), texinputs=[current_dir, ''])
+    pdf.save_to('generated_pdf.pdf')
+
+
+if __name__ == '__main__':
+    building_pdf()
+
