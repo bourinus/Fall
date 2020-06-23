8f46b2f91befe677afe8c1759f2dd223ab6cdc26
sh4d0w <mehdi.belahcene@outlook.fr>
2020-06-23 11:33:37 +0200

    remove attr
    Signed-off-by: sh4d0w <mehdi.belahcene@outlook.fr>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,9 +55,9 @@ extensions = [
 ]
 
 #https://stackoverflow.com/questions/46278683/include-my-markdown-readme-into-sphinx
-pyf
+
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_static/css/']
+#templates_path = ['_static/css/']
 
 #import spec
 #This theme is a dark theme for sphinx documentation based on the read the docs theme.
@@ -108,7 +108,7 @@ html_theme_options = {}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['/_static/css/pdj.css']
+#html_static_path = ['/_static/css/pdj.css']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
